38e11c888420b86b0a64cf958b0a3267ce5e6f4e
t_how <taylerhow@macintosh.wlan.rose-hulman.edu>
2015-03-26 19:49:46 -0400

    Added basic GUI window
    Running the Main file now creates and displays a JFrame for our GUI.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import javax.swing.JFrame;
 
 public class Main {
 
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 600);
+		frame.setLocation(100, 100);
+		frame.setTitle("Welcome to Arimaa!");
+		frame.setVisible(true);
+
+	}
 }
