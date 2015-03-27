b36b021efb1b26ba0cb034e81d5ea09f0f575f8a
Jernik <Luke.Miller.DRSS@gmail.com>
2015-03-26 21:47:08 -0400

    Added Game classs

---
+++ src/Game.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+
+public class Game {
+	//fields
+	ArrayList<BoardState> boards= new ArrayList<BoardState>();
+	BoardState currentBoard=null;
+	int numMovesLeft =0;
+	int moveTimer =0;
+	int p1TimeBank=0;
+	int p2TimeBank =0;
+	/**
+	 * 
+	 */
+	public void render(){}
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public Piece getSpace(int x, int y){
+		return null;
+	}
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param dir 0: up, 1: right, 2: down, 3: left
+	 * @return
+	 */
+	public boolean move(int x, int y, int dir){
+		return false;
+	}
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param dir1
+	 * @param dir2
+	 * @return
+	 */
+	public boolean push(int x, int y, int dir1, int dir2){
+		return false;
+	}
+	/**
+	 * 
+	 * @param x
+	 * @param y
+	 * @param dir1
+	 * @param dir2
+	 * @return
+	 */
+	public boolean pull(int x, int y, int dir1, int dir2){
+		return false;
+	}
+}
