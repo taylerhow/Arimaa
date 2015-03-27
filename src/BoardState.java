31d8d9b7612d5fa6a79b85f4b2d8edf052120488
Jesse Shellabarger <shellajt@rose-hulman.edu>
2015-03-26 21:48:46 -0400

    Added BoardState class shell

--- src/BoardState.java
+++ src/BoardState.java
@@ -1,11 +1,21 @@
-/**
- * 
- */
+import java.util.ArrayList;
 
 /**
+ * This class represents the states of the board.
+ * 
  * @author shellajt
  *
  */
 public class BoardState {
-
+	//Fields
+	char[][] boardArray;
+	int turnNumber;
+	
+	public BoardState(char[][] map, int turnNumber){
+		
+	}
+	
+	public BoardState(BoardState prevState, ArrayList<String> moveList){
+		
+	}
 }
