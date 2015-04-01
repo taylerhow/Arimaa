5108945f78eeaaa58e8544249f91321a4fdd9ec5
Tayler How <taylerhow@gmail.com>
2015-03-31 20:02:03 -0400

    added second GUI initialization test
    added testThatGUIInitializesWithCorredtDefaultValues, which verifies
    that the values set for p1name, p2name, and mainFrame are correct upon
    initialization. This test currently fails.

--- src/testing/TestGUI.java
+++ src/testing/TestGUI.java
@@ -1,6 +1,9 @@
 package testing;
 
 import static org.junit.Assert.*;
+
+import javax.swing.JFrame;
+
 import game.GUI;
 
 import org.junit.Test;
@@ -12,5 +15,14 @@ public class TestGUI {
 		GUI testGUI = new GUI();
 		assertNotEquals(testGUI, null);
 	}
-
+	
+	@Test
+	public void testThatGUIInitializesWithCorrectDefaultValues() {
+		GUI testGUI = new GUI();
+		assertEquals(testGUI.getP1name(), "Player 1");
+		assertEquals(testGUI.getP1name(), "Player 2");
+		assertNotNull(testGUI.getMainFrame());
+		assertEquals(testGUI.getMainFrame().getTitle(), "Welcome to Arimaa!");
+		assertEquals(testGUI.getMainFrame().getDefaultCloseOperation(), JFrame.EXIT_ON_CLOSE);
+	}
 }
