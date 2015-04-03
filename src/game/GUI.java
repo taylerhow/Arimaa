c88b88cae54d0a61cf0dc5d432f6ae312c4b097c
Tayler How <taylerhow@gmail.com>
2015-04-02 22:21:18 -0400

    New Game button opens new window
    Pressing the New Game button in the main menu now closes the main menu
    and opens a new window, purposed for playing the game.

--- src/game/GUI.java
+++ src/game/GUI.java
@@ -6,53 +6,62 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 
 public class GUI {
 	private String p1name;
 	private String p2name;
-	private JFrame mainFrame;
-	
-	public GUI(){
+	private JFrame activeFrame;
+
+	public GUI() {
 		this.p1name = "Player 1";
 		this.p2name = "Player 2";
-		this.mainFrame = new JFrame();
-		this.mainFrame.setTitle("Welcome to Arimaa!");
-		this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JFrame mainMenuFrame = new JFrame();
+		this.activeFrame = mainMenuFrame;
+		mainMenuFrame.setTitle("Welcome to Arimaa!");
+		mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
-	public static void main(String[] args) {
-	    ImagePanel panel = new ImagePanel(new ImageIcon("resources/board.jpg").getImage());
 
-	    JFrame frame = new JFrame();
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setTitle("Welcome to Arimaa!");
-	    frame.getContentPane().add(panel);
-	    frame.pack();
-	    
-	    JButton newGameButton = new JButton();
-//	    newGameButton.setBackground(Color.DARK_GRAY);
-	    newGameButton.setSize(100, 50);
-	    newGameButton.setText("New Game");
-	    newGameButton.setLocation((panel.getWidth()/4)-35, (panel.getHeight()/2)-25);
-	    panel.add(newGameButton);
-	    
-	    JButton loadGameButton = new JButton();
-//	    loadGameButton.setBackground(Color.DARK_GRAY);
-	    loadGameButton.setSize(100, 50);
-	    loadGameButton.setText("Load Game");
-	    loadGameButton.setLocation((panel.getWidth()/4)*3-65, (panel.getHeight()/2)-25);
-	    panel.add(loadGameButton);
-	    
-	    frame.setVisible(true);
-	    panel.setVisible(true);
-	    newGameButton.setVisible(true);
-	    loadGameButton.setVisible(true);
-	  }
+	public static void main(String[] args) {
+		GUI g = new GUI();
+
+		// Add MAIN MENU panel with appropriate background image
+		ImagePanel panel = g.new ImagePanel(
+				new ImageIcon("resources/board.jpg").getImage());
+		g.activeFrame.getContentPane().add(panel);
+		g.activeFrame.pack();
+		panel.setVisible(true);
+
+		// Add the NEW GAME button to the Main Menu
+		JButton newGameButton = new JButton();
+		newGameButton.setSize(100, 50);
+		newGameButton.setText("New Game");
+		newGameButton.setLocation((panel.getWidth() / 4) - 35,
+				(panel.getHeight() / 2) - 25);
+		panel.add(newGameButton);
+		newGameButton.setVisible(true);
+
+		// Setup ActionListener for NEW GAME button
+		newGameButton.addActionListener(g.new newGameListener());
+
+		// Add the LOAD GAME button to the Main Menu
+		JButton loadGameButton = new JButton();
+		loadGameButton.setSize(100, 50);
+		loadGameButton.setText("Load Game");
+		loadGameButton.setLocation((panel.getWidth() / 4) * 3 - 65,
+				(panel.getHeight() / 2) - 25);
+		panel.add(loadGameButton);
+		loadGameButton.setVisible(true);
+
+		g.activeFrame.setVisible(true);
+	}
 
 	public String getP1name() {
 		return p1name;
@@ -70,35 +79,48 @@ public class GUI {
 		this.p2name = p2name;
 	}
 
-	public JFrame getMainFrame() {
-		return mainFrame;
+	public JFrame getActiveFrame() {
+		return activeFrame;
 	}
 
-	public void setMainFrame(JFrame mainFrame) {
-		this.mainFrame = mainFrame;
-	}
+	public void setActiveFrame(JFrame activeFrame) {
+		this.activeFrame = activeFrame;
 	}
 
+	class newGameListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			JFrame gameFrame = new JFrame();
+			activeFrame.dispose();
+			activeFrame = gameFrame;
+			gameFrame.setSize(400, 400);
+			gameFrame.setTitle("Arimaa");
+			gameFrame.setVisible(true);
+		}
+	}
+	
 	class ImagePanel extends JPanel {
 
-	  private Image img;
+		private Image img;
 
-	  public ImagePanel(String img) {
-	    this(new ImageIcon(img).getImage());
-	  }
+		public ImagePanel(String img) {
+			this(new ImageIcon(img).getImage());
+		}
 
-	  public ImagePanel(Image img) {
-	    this.img = img;
-	    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
-	    setPreferredSize(size);
-	    setMinimumSize(size);
-	    setMaximumSize(size);
-	    setSize(size);
-	    setLayout(null);
-	  }
+		public ImagePanel(Image img) {
+			this.img = img;
+			Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+			setPreferredSize(size);
+			setMinimumSize(size);
+			setMaximumSize(size);
+			setSize(size);
+			setLayout(null);
+		}
 
-	  public void paintComponent(Graphics g) {
-	    g.drawImage(img, 0, 0, null);
-	  }
+		public void paintComponent(Graphics g) {
+			g.drawImage(img, 0, 0, null);
+		}
 
 	}
+}
