07c2b20aec8761414139a76b783de4d963d6264c
Jernik <Luke.Miller.DRSS@gmail.com>
2015-04-02 22:24:05 -0400

    Added tests

--- src/testing/TestGame.java
+++ src/testing/TestGame.java
@@ -1,6 +1,18 @@
 package testing;
 import static org.junit.Assert.*;
-import game.Game;
+import game.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+ 
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -12,5 +24,12 @@ public class TestGame {
 		Game g =new Game();
 		assertNotNull(g);
 	}
+	
+	@Test
+	public void testMove(){
+		BoardState b = new BoardState(null, 0);
+		Game g= new Game(b);
+	}
 
 }
+
