f7b7fde5c9cf7fe6c01b12fe50250101d840b942
Tayler How <taylerhow@gmail.com>
2015-04-16 21:08:06 -0400

    Fixed(?) some of the move and switch piece logic
    Also changed x & y to row & column because that was confusing..

--- src/game/Game.java
+++ src/game/Game.java
@@ -50,36 +50,40 @@ public class Game {
 	}
 	/**
 	 * 
-	 * @param x
-	 * @param y
+	 * @param row
+	 * @param column
 	 * @param dir 0: up, 1: right, 2: down, 3: left
 	 * @return
 	 */
-	public boolean move(int x, int y, int dir){
+	public boolean move(int row, int column, int dir){
 		boards.add(currentBoard);
 		currentBoard=currentBoard.clone();
 		switch(dir){
 		case 0:
-			if (x - 1 >= 0) {
-				switchPiece(x, y, x-1, y);
+			//Moving UP
+			if (row - 1 >= 0) {
+				switchPiece(row, column, row-1, column);
 				return true;
 			}
 			return false;
 		case 1:
-			if (y + 1 < 8) {
-				switchPiece(x, y, x, y+1);
+			//Moving RIGHT
+			if (column + 1 <= 7) {
+				switchPiece(row, column, row, column+1);
 				return true;
 			}
 			return false;
 		case 2:
-			if (x + 1 < 8) {
-				switchPiece(x, y, x + 1, y);
+			//Moving DOWN
+			if (row + 1 <= 7) {
+				switchPiece(row, column, row + 1, column);
 				return true;
 			}
 			return false;
 		case 3:
-			if (y - 1 >= 0) {
-				switchPiece(x, y, x, y - 1);
+			//Moving LEFT
+			if (column - 1 >= 0) {
+				switchPiece(row, column, row, column - 1);
 				return true;
 			}
 			return false;
@@ -88,14 +92,14 @@ public class Game {
 		}
 	}
 	//helper for move
-	private void switchPiece(int x, int y, int x2, int y2) {
+	private void switchPiece(int row1, int column1, int row2, int column2) {
 		//System.out.println(x+" "+ y+"  "+x2+" "+y2);
 		char[][] boardArray = currentBoard.getBoardArray();
 //		System.out.println(Arrays.deepToString(boardArray));
-		char temp = boardArray[y][x];
+		char temp = boardArray[row1][column1];
 		//System.out.println(temp+" "	+boardArray[y][x]+" "+boardArray[y2][x2]);
-		boardArray[y][x]=boardArray[y2][x2];
-		boardArray[y2][x2]=temp;
+		boardArray[row1][column1]=boardArray[row2][column2];
+		boardArray[row2][column2]=temp;
 
 //		System.out.println(Arrays.deepToString(boardArray));
 		//System.out.println("SWITCH "+temp+" "	+boardArray[x][y]+" "+boardArray[x2][y2]);
