493bf0278f12fccadecb3cf2b05d6c8aace5184b
Tayler How <taylerhow@gmail.com>
2015-04-30 17:25:45 -0400

    Added testBidirectionalPullLeftUp()
    Fails

--- src/testing/TestGame.java
+++ src/testing/TestGame.java
@@ -444,6 +444,16 @@ public class TestGame {
 	}
 	
 	@Test
+	public void testBidirectionalPullLeftUp(){
+		Game g = new Game(pullTestingBoard);
+		Piece p1 = g.getSpace(5, 5);
+		Piece p2 = g.getSpace(6, 5);
+		assertTrue(g.pull(5, 5, 6, 5, 2));
+		assertTrue(g.getSpace(5, 4).equals(p1));
+		assertTrue(g.getSpace(5, 5).equals(p2));
+	}
+	
+	@Test
 	public void testPullPieceOfGreaterStrength(){
 		Game g = new Game(pullTestingBoard);
 		assertFalse(g.pull(7, 3, 7, 4, 3));
