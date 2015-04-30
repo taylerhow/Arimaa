75904894573a0eeae876db11cca1b1d488a4edb6
Tayler How <taylerhow@gmail.com>
2015-04-30 17:24:45 -0400

    Passed testBidirectionalPullDownLeft()

--- src/game/Game.java
+++ src/game/Game.java
@@ -196,8 +196,8 @@ public class Game {
 			}
 			break;
 		case 2:
-			if(getSpace(row1-1, column1)!=null && getSpace(row1, column1).getOwner() != getSpace(row1-1, column1).getOwner() && move(row1, column1, direction1)){
-				move(row1-1, column1, direction1);
+			if(getSpace(row1, column1).getOwner() != getSpace(row2, column2).getOwner() && move(row1, column1, direction1)){
+				move(row2, column2, direction2);
 				return true;
 			}
 			break;
