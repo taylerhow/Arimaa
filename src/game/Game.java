ae51b1d65a5cd0bcfd6171d437fbc0a025f4680f
Jernik <Luke.Miller.DRSS@gmail.com>
2015-05-08 14:15:54 -0400

    Control+shift+f

--- src/game/Game.java
+++ src/game/Game.java
@@ -12,19 +12,18 @@ public class Game {
 	// fields
 	ArrayList<BoardState> boards = new ArrayList<BoardState>();
 	public BoardState currentBoard = null;
-	//int numMovesLeft = 0;
+	// int numMovesLeft = 0;
 	int moveTimer = 0;
 	int p1TimeBank = 0;
 	int p2TimeBank = 0;
 	int turnCounter = 0;
-	String p1Name= "";
+	String p1Name = "";
 	String p2Name = "";
-	//0 is noone, 1 is player1, 2 is player2
-	private int winner=0;
-	private int numMoves=4;
-	private int playerTurn=1;
+	// 0 is noone, 1 is player1, 2 is player2
+	private int winner = 0;
+	private int numMoves = 4;
+	private int playerTurn = 1;
 	private boolean isPushPull;
-
 
 	public Game(BoardState b) {
 		currentBoard = b;
@@ -69,14 +68,14 @@ public class Game {
 	 * @return
 	 */
 	public boolean move(int row, int column, int dir) {
-		if(!isValidMoveFromSquare(row, column))
+		if (!isValidMoveFromSquare(row, column))
 			return false;
 		boards.add(currentBoard);
 		currentBoard = currentBoard.clone();
 		switch (dir) {
 		case 0:
 			// Moving UP
-			if (isValidMoveSquare(row-1, column)) {
+			if (isValidMoveSquare(row - 1, column)) {
 				switchPiece(row, column, row - 1, column);
 				endMove();
 				return true;
@@ -84,7 +83,7 @@ public class Game {
 			return false;
 		case 1:
 			// Moving RIGHT
-			if (isValidMoveSquare(row, column+1)) {
+			if (isValidMoveSquare(row, column + 1)) {
 				switchPiece(row, column, row, column + 1);
 				endMove();
 				return true;
@@ -92,7 +91,7 @@ public class Game {
 			return false;
 		case 2:
 			// Moving DOWN
-			if (isValidMoveSquare(row+1, column)) {
+			if (isValidMoveSquare(row + 1, column)) {
 				switchPiece(row, column, row + 1, column);
 				endMove();
 				return true;
@@ -100,7 +99,7 @@ public class Game {
 			return false;
 		case 3:
 			// Moving LEFT
-			if (isValidMoveSquare(row, column-1)) {
+			if (isValidMoveSquare(row, column - 1)) {
 				switchPiece(row, column, row, column - 1);
 				endMove();
 				return true;
@@ -116,16 +115,19 @@ public class Game {
 			return false;
 		// This may cause issues when we implement undo/redo if we try invalid
 		// moves before we undo
-		if(getSpace(row, column).getOwner()!=Owner.values()[(getPlayerTurn()-1)]&&!isPushPull)
-			return false;//not your turn
-		
-		if((checkStrongerAdjacent(row, column)&&!checkFriendlyAdjacent(row, column))&&!isPushPull)
-			return false;//can't move
+		if (getSpace(row, column).getOwner() != Owner.values()[(getPlayerTurn() - 1)]
+				&& !isPushPull)
+			return false;// not your turn
+
+		if ((checkStrongerAdjacent(row, column) && !checkFriendlyAdjacent(row,
+				column)) && !isPushPull)
+			return false;// can't move
 		return true;
 	}
 
 	private boolean isValidMoveSquare(int row, int column) {
-		if(row >= 0&&row<8&&column>=0&&column<8&&currentBoard.getBoardArray()[row][column] == ' ')
+		if (row >= 0 && row < 8 && column >= 0 && column < 8
+				&& currentBoard.getBoardArray()[row][column] == ' ')
 			return true;
 		return false;
 	}
@@ -153,26 +155,29 @@ public class Game {
 		}
 	}
 
-	//This method checks both rows for rabbits of the opposite side
+	// This method checks both rows for rabbits of the opposite side
 	private void checkWin() {
-		//check top row
-		for(int i=0;i<8;i++){
-			if(getSpace(0, i)!=null){
-				if(getSpace(0, i).equals(new Piece(PieceType.Rabbit, null, Piece.Owner.Player2))){
-					winner=2;
+		// check top row
+		for (int i = 0; i < 8; i++) {
+			if (getSpace(0, i) != null) {
+				if (getSpace(0, i).equals(
+						new Piece(PieceType.Rabbit, null, Piece.Owner.Player2))) {
+					winner = 2;
 				}
 			}
 		}
-		//check bottom row
-		for(int i=0;i<8;i++){
-			if(getSpace(7, i)!=null){
-				if(getSpace(7, i).equals(new Piece(PieceType.Rabbit, null, Piece.Owner.Player1))){
-					winner=1;
+		// check bottom row
+		for (int i = 0; i < 8; i++) {
+			if (getSpace(7, i) != null) {
+				if (getSpace(7, i).equals(
+						new Piece(PieceType.Rabbit, null, Piece.Owner.Player1))) {
+					winner = 1;
 				}
 			}
 		}
-		//noone has won
-		if(winner!=0) System.out.println("Winner: "+ winner);
+		// noone has won
+		if (winner != 0)
+			System.out.println("Winner: " + winner);
 	}
 
 	/**
@@ -181,69 +186,69 @@ public class Game {
 	 * 
 	 */
 	private void checkDeaths(int row, int col) {
-		if(this.getSpace(row, col)==(null))
-			return;//an empty piece doesn't need to be checked
-		
-		if(checkFriendlyAdjacent(row, col)){
+		if (this.getSpace(row, col) == (null))
+			return;// an empty piece doesn't need to be checked
+
+		if (checkFriendlyAdjacent(row, col)) {
 			return;
 		}
-		//no adjacent friendly pieces, remove this one
-		char[][] temp= this.currentBoard.getBoardArray();
-		temp[row][col]=' ';
+		// no adjacent friendly pieces, remove this one
+		char[][] temp = this.currentBoard.getBoardArray();
+		temp[row][col] = ' ';
 		this.currentBoard.setBoardArray(temp);
 	}
 
 	private boolean checkFriendlyAdjacent(int row, int col) {
-		Piece cen=this.getSpace(row, col);
-		Piece up=this.getSpace(row-1, col);
-		Piece down=this.getSpace(row+1, col);
-		Piece left=this.getSpace(row, col-1);
-		Piece right = this.getSpace(row, col+1);
-		Owner own =cen.getOwner();
-		if(up!=null){
-			if(up.getOwner()==own)
+		Piece cen = this.getSpace(row, col);
+		Piece up = this.getSpace(row - 1, col);
+		Piece down = this.getSpace(row + 1, col);
+		Piece left = this.getSpace(row, col - 1);
+		Piece right = this.getSpace(row, col + 1);
+		Owner own = cen.getOwner();
+		if (up != null) {
+			if (up.getOwner() == own)
 				return true;
 		}
-		if(down!=null){
-			if(down.getOwner()==own)
+		if (down != null) {
+			if (down.getOwner() == own)
 				return true;
 		}
-		if(right!=null){
-			if(right.getOwner()==own)
+		if (right != null) {
+			if (right.getOwner() == own)
 				return true;
 		}
-		if(left!=null){
-			if(left.getOwner()==own)
+		if (left != null) {
+			if (left.getOwner() == own)
 				return true;
 		}
 		return false;
 	}
-	
+
 	private boolean checkStrongerAdjacent(int row, int col) {
-		Piece cen=this.getSpace(row, col);
-		Piece up=this.getSpace(row-1, col);
-		Piece down=this.getSpace(row+1, col);
-		Piece left=this.getSpace(row, col-1);
-		Piece right = this.getSpace(row, col+1);
-		Owner own =cen.getOwner();
-		boolean foo=false;
-		if(up!=null){
-			foo=checkStrong(up,cen);
+		Piece cen = this.getSpace(row, col);
+		Piece up = this.getSpace(row - 1, col);
+		Piece down = this.getSpace(row + 1, col);
+		Piece left = this.getSpace(row, col - 1);
+		Piece right = this.getSpace(row, col + 1);
+		Owner own = cen.getOwner();
+		boolean foo = false;
+		if (up != null) {
+			foo = checkStrong(up, cen);
 		}
-		if(down!=null){
-			foo=checkStrong(down, cen);
+		if (down != null) {
+			foo = checkStrong(down, cen);
 		}
-		if(right!=null){
-			foo=checkStrong(right,cen);
+		if (right != null) {
+			foo = checkStrong(right, cen);
 		}
-		if(left!=null){
-			foo=checkStrong(left,cen);
+		if (left != null) {
+			foo = checkStrong(left, cen);
 		}
 		return foo;
 	}
 
 	private boolean checkStrong(Piece one, Piece two) {
-		if(one.getOwner()!=two.getOwner()&&one.isStrongerThan(two))
+		if (one.getOwner() != two.getOwner() && one.isStrongerThan(two))
 			return true;
 		return false;
 	}
@@ -271,42 +276,31 @@ public class Game {
 	 * @return
 	 */
 	public boolean push(int row, int column, int dir1, int dir2) {
-		if(numMoves<=1)
-			return false; //can't push/pull with only one move
-		if (getSpace(row, column) == null) {
-			return false; // trying to push with an empty square
-		}
-		if(getSpace(row, column).getOwner()!=Owner.values()[(getPlayerTurn()-1)]&&!isPushPull)
-			return false;//not your turn
-		isPushPull=true;
+		if (!isValidSquareToPushFrom(row, column))
+			return false;
+		isPushPull = true;
 		switch (dir1) {
 		case 0:
 			if (row - 1 >= 0) {
 				Piece pushingPiece = getSpace(row, column);
 				Piece pushedPiece = getSpace(row - 1, column);
-				if (pushedPiece != null
-						&& pushingPiece.isStrongerThan(pushedPiece)) {
-					if (pushingPiece.getOwner() != pushedPiece.getOwner()
-							&& move(row - 1, column, dir2)) {
-						isPushPull=false;
-						// should always be true
-						return move(row, column, dir1);
-					}
+				if (pieceCanPush(pushingPiece, pushedPiece)&& move(row - 1, column, dir2)) {
+					isPushPull = false;
+					// should always be true
+					return move(row, column, dir1);
 				}
 			}
+
 			break;
 		case 1:
 			if (column + 1 <= 7) {
 				Piece pushingPiece2 = getSpace(row, column);
 				Piece pushedPiece2 = getSpace(row, column + 1);
-				if (pushedPiece2 != null
-						&& pushingPiece2.isStrongerThan(pushedPiece2)) {
-					if (pushingPiece2.getOwner() != pushedPiece2.getOwner()
-							&& move(row, column + 1, dir2)) {
-						isPushPull=false;
-						// should always be true
-						return move(row, column, dir1);
-					}
+				if (pieceCanPush(pushingPiece2, pushedPiece2)&& move(row, column + 1, dir2)) {
+					isPushPull = false;
+					// should always be true
+					return move(row, column, dir1);
+
 				}
 			}
 			break;
@@ -315,11 +309,11 @@ public class Game {
 				Piece pushingPiece3 = getSpace(row, column);
 				Piece pushedPiece3 = getSpace(row + 1, column);
 				if (pushingPiece3.isStrongerThan(pushedPiece3)) {
-					if (pushingPiece3.getOwner() != pushedPiece3.getOwner()
-							&& move(row + 1, column, dir2)) {
-						isPushPull=false;
+					if (pieceCanPush(pushingPiece3, pushedPiece3)&& move(row + 1, column, dir2)) {
+						isPushPull = false;
 						// should always be true
 						return move(row, column, dir1);
+
 					}
 				}
 			}
@@ -328,18 +322,33 @@ public class Game {
 			if (column - 1 >= 0) {
 				Piece pushingPiece4 = getSpace(row, column);
 				Piece pushedPiece4 = getSpace(row, column - 1);
-				if (pushingPiece4.isStrongerThan(pushedPiece4)) {
-					if (pushingPiece4.getOwner() != pushedPiece4.getOwner()
-							&& move(row, column - 1, dir2)) {
-						isPushPull=false;
-						// should always be true
-						return move(row, column, dir1);
-					}
+				if (pieceCanPush(pushingPiece4, pushedPiece4)&& move(row, column - 1, dir2)) {
+					isPushPull = false;
+					// should always be true
+					return move(row, column, dir1);
 				}
 			}
+
 			break;
 		}
 		return false;
+	}
+
+	private boolean pieceCanPush(Piece pushingPiece, Piece pushedPiece) {
+		return pushedPiece != null && pushingPiece.isStrongerThan(pushedPiece)
+				&& pushingPiece.getOwner() != pushedPiece.getOwner();
+	}
+
+	private boolean isValidSquareToPushFrom(int row, int column) {
+		if (numMoves <= 1)
+			return false; // can't push/pull with only one move
+		if (getSpace(row, column) == null) {
+			return false; // trying to push with an empty square
+		}
+		if (getSpace(row, column).getOwner() != Owner.values()[(getPlayerTurn() - 1)]
+				&& !isPushPull)
+			return false;// not your turn
+		return true;
 	}
 
 	/**
@@ -354,38 +363,39 @@ public class Game {
 	 * @param direction2
 	 *            : direction the piece being pulled will move
 	 * @return True if pull succeeds, False if it fails
-	 */ 
+	 */
 	public boolean pull(int row1, int column1, int row2, int column2,
 			int direction1) {
-		if(numMoves<2)
-			return false; //can't push/pull with only one move
+		if (numMoves < 2)
+			return false; // can't push/pull with only one move
 		// Check that both pieces exist
-		if (getSpace(row1, column1) == null || getSpace(row2, column2) == null){
-			//System.out.println("Null piece");
-			return false;
-			}
-
-		// Check that pulling piece is strong than other piece
-		if (!getSpace(row1, column1).isStrongerThan(getSpace(row2, column2))){
-			//System.out.println("Weak piece");
+		if (getSpace(row1, column1) == null || getSpace(row2, column2) == null) {
+			// System.out.println("Null piece");
 			return false;
 		}
-		
-		if(getSpace(row1, column1).getOwner()!=Owner.values()[(getPlayerTurn()-1)]&&!isPushPull)
-			return false;//not your turn
+
+		// Check that pulling piece is strong than other piece
+		if (!getSpace(row1, column1).isStrongerThan(getSpace(row2, column2))) {
+			// System.out.println("Weak piece");
+			return false;
+		}
+
+		if (getSpace(row1, column1).getOwner() != Owner.values()[(getPlayerTurn() - 1)]
+				&& !isPushPull)
+			return false;// not your turn
 		// Get direction that pulled piece will move
 		int direction2 = getDirection(row2, column2, row1, column1);
 
 		// Check that getDirection didn't fail
 		// if(direction2 == -1) return false;
-		isPushPull=true;
+		isPushPull = true;
 		// Attempt to perform move operations on both pieces
 		switch (direction1) {
 		case 0:
 			if (getSpace(row1, column1).getOwner() != getSpace(row2, column2)
 					.getOwner() && move(row1, column1, direction1)) {
 				move(row2, column2, direction2);
-				isPushPull=false;
+				isPushPull = false;
 				return true;
 			}
 			break;
@@ -393,7 +403,7 @@ public class Game {
 			if (getSpace(row1, column1).getOwner() != getSpace(row2, column2)
 					.getOwner() && move(row1, column1, direction1)) {
 				move(row2, column2, direction2);
-				isPushPull=false;
+				isPushPull = false;
 				return true;
 			}
 			break;
@@ -401,7 +411,7 @@ public class Game {
 			if (getSpace(row1, column1).getOwner() != getSpace(row2, column2)
 					.getOwner() && move(row1, column1, direction1)) {
 				move(row2, column2, direction2);
-				isPushPull=false;
+				isPushPull = false;
 				return true;
 			}
 			break;
@@ -409,7 +419,7 @@ public class Game {
 			if (getSpace(row1, column1).getOwner() != getSpace(row2, column2)
 					.getOwner() && move(row1, column1, direction1)) {
 				move(row2, column2, direction2);
-				isPushPull=false;
+				isPushPull = false;
 				return true;
 			}
 			break;
@@ -448,7 +458,7 @@ public class Game {
 	}
 
 	public boolean loadFile(Scanner scanner) {
-		//Setup to use Scanner
+		// Setup to use Scanner
 		scanner.useDelimiter(",");
 		BoardState boardToSet = new BoardState(new char[][] {
 				{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
@@ -459,13 +469,14 @@ public class Game {
 				{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
 				{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
 				{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }, }, 0);
-		String[] validBoardCharactersArray = {" ", "E", "C", "H", "D", "K", "R", "e", "c", "h", "d", "k", "r"};
+		String[] validBoardCharactersArray = { " ", "E", "C", "H", "D", "K",
+				"R", "e", "c", "h", "d", "k", "r" };
 		ArrayList<String> vbc = new ArrayList<String>();
-		for(String s : validBoardCharactersArray){
+		for (String s : validBoardCharactersArray) {
 			vbc.add(s);
 		}
-		
-		//Parse boardState
+
+		// Parse boardState
 		for (int i = 0; i < 8; i++) {
 			for (int k = 0; k < 8; k++) {
 				if (!scanner.hasNext()) {
@@ -483,13 +494,13 @@ public class Game {
 			}
 		}
 
-		//Parse turnCounter, p1Name, p2Name
+		// Parse turnCounter, p1Name, p2Name
 		if (!scanner.hasNext()) {
 			scanner.close();
 			System.out.println("3");
 			return false;
 		}
-		 int turnCounter = scanner.nextInt();
+		int turnCounter = scanner.nextInt();
 		if (!scanner.hasNext()) {
 			scanner.close();
 			System.out.println("4");
@@ -503,8 +514,8 @@ public class Game {
 		}
 		String p2name = scanner.next();
 		scanner.close();
-		
-		//Successful load! Push all changes to game permanently
+
+		// Successful load! Push all changes to game permanently
 		this.currentBoard = boardToSet;
 		this.turnCounter = turnCounter;
 		this.p1Name = p1name;
@@ -523,7 +534,8 @@ public class Game {
 				}
 			}
 		}
-		if(fw == null) return false;
+		if (fw == null)
+			return false;
 		String s2 = "" + this.turnCounter + ",";
 
 		try {
@@ -550,8 +562,8 @@ public class Game {
 	public String getP2Name() {
 		return this.p2Name;
 	}
-	
-	public void setWinner(int winner){
+
+	public void setWinner(int winner) {
 		this.winner = winner;
 	}
 
@@ -570,7 +582,8 @@ public class Game {
 	}
 
 	/**
-	 * @param playerTurn the playerTurn to set
+	 * @param playerTurn
+	 *            the playerTurn to set
 	 */
 	public void setPlayerTurn(int playerTurn) {
 		this.playerTurn = playerTurn;
