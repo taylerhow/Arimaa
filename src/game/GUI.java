b44ad9b5576b3c677ee4ccce4ba42148ca6005ff
Tayler How <taylerhow@gmail.com>
2015-05-14 18:51:53 -0400

    Removed outdated files

--- src/game/GUI.java
+++ src/game/GUI.java
@@ -1,15 +1,6 @@
 package game;
 
 import java.awt.Color;
-
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
@@ -18,6 +9,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -25,22 +24,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import javax.swing.ImageIcon;
-
 public class GUI {
 	private String p1Name;
 	private String p2Name;
-	private JTextField p1TextField;
-	private JTextField p2TextField;
-	private JComboBox<Integer> timerComboBox;
 	private ArrayList<JFrame> activeFrames;
 	private Game game;
 	private ImagePanel gameBoardPanel = null;
 	private ImagePanel[][] boardPieces;
-
-	// private int numMoves;
-	// private int playerTurn;
-
+	private JTextField p1TextField;
+	private JTextField p2TextField;
+	private JComboBox<Integer> timerComboBox;
 	private JLabel moveCountLabel;
 	private JLabel turnCountLabel;
 	private JLabel turnIndicatorLabel;
