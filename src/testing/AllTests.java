f0f48288512edbaafdf87046e2a363843e1ebadf
Jesse Shellabarger <shellajt@rose-hulman.edu>
2015-05-20 15:30:50 -0400

    CodeCoverage improvements

--- src/testing/AllTests.java
+++ src/testing/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TestBoardState.class, TestGame.class, TestPiece.class })
+@Suite.SuiteClasses({ TestBoardState.class, TestGame.class, TestPiece.class, TestTimePanel.class })
 public class AllTests {
 	public static void main(String args[]) {
 		org.junit.runner.JUnitCore.main("testing.AllTests");
--- src/testing/TestTimePanel.java
+++ src/testing/TestTimePanel.java
@@ -2,13 +2,20 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import javax.swing.JLabel;
+
+import game.GUI;
+import game.Game;
+import game.TimePanel;
+
 import org.junit.Test;
 
 public class TestTimePanel {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testInitializes() {
+		TimePanel tp = new TimePanel(new GUI(), new Game(), 15, new JLabel());
+		assertNotNull(tp);
 	}
 
 }
