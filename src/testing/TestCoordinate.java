5994c58f2c97cc5690288d62ec8fc652c2ff0658
Philip Ross <rosspa@rose-hulman.edu>
2016-04-13 11:11:40 -0400

    coordinate testing code

--- src/testing/TestCoordinate.java
+++ src/testing/TestCoordinate.java
@@ -1,8 +1,6 @@
 package testing;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -14,53 +12,110 @@ public class TestCoordinate {
 		Coordinate c = new Coordinate(0, 0);
 		assertNotNull(c);
 	}
-	
+
 	@Test
 	public void testValidCoordinateIsValid() {
 		Coordinate c = new Coordinate(0, 0);
 		assertTrue(c.isValid());
+
+		c = new Coordinate(5, 3);
+		assertTrue(c.isValid());
+		
+		c = new Coordinate(7, 7);
+		assertTrue(c.isValid());
+	}
+
+	@Test
+	public void testCanGetX() {
+		Coordinate c = new Coordinate(1, 2);
+		assertEquals(1, c.getX());
+	}
+	
+	@Test
+	public void testCanGetY() {
+		Coordinate c = new Coordinate(1, 2);
+		assertEquals(2, c.getY());
 	}
 	
 	@Test
 	public void testInvalidCoordinateIsInvalid() {
 		Coordinate c = new Coordinate(-1, 0);
 		assertFalse(c.isValid());
-		
+
 		c = new Coordinate(0, -1);
 		assertFalse(c.isValid());
-		
+
 		c = new Coordinate(-1, -1);
 		assertFalse(c.isValid());
-		
+
 		c = new Coordinate(8, 0);
 		assertFalse(c.isValid());
-		
+
 		c = new Coordinate(0, 8);
 		assertFalse(c.isValid());
-		
+
 		c = new Coordinate(8, 8);
 		assertFalse(c.isValid());
+		
+		c = new Coordinate(-1, 8);
+		assertFalse(c.isValid());
 	}
-	
+
 	@Test
 	public void testNullEquality() {
-		assertFalse(new Coordinate(0, 0).equals(null));
+		assertNotEquals(new Coordinate(0, 0), null);
 	}
-	
+
 	@Test
 	public void testReflexiveEquality() {
 		Coordinate c = new Coordinate(0, 0);
-		assertTrue(c.equals(c));		
+		assertEquals(c, c);
 	}
-	
+
 	@Test
 	public void testSymetricEquality() {
 		Coordinate a = new Coordinate(0, 0);
 		Coordinate b = new Coordinate(0, 0);
-		assertTrue(a.equals(b));
-		assertTrue(b.equals(a));
-		
+		assertEquals(a, b);
+		assertEquals(b, a);
 	}
 
+	@Test
+	public void testTransitiveEquality() {
+		Coordinate a = new Coordinate(0, 0);
+		Coordinate b = new Coordinate(0, 0);
+		Coordinate c = new Coordinate(0, 0);
+		assertEquals(a, b);
+		assertEquals(b, c);
+		assertEquals(a, c);
+	}
+
+	@Test
+	public void testInvalidIsNeverEqual() {
+		Coordinate valid = new Coordinate(0, 0);
+		Coordinate invalid1 = new Coordinate(-1, -1);
+		Coordinate invalid2 = new Coordinate(8, 8);
+		
+		assertNotEquals(valid, invalid1);
+		assertNotEquals(valid, invalid2);
+		assertNotEquals(invalid1, valid);
+		assertNotEquals(invalid2, valid);
+		
+		assertNotEquals(invalid1, invalid1);
+		assertNotEquals(invalid2, invalid2);
+	}
+	
+	@Test
+	public void testPositiveEquality() {
+		Coordinate a = new Coordinate(0, 0);
+		Coordinate b = new Coordinate(0, 0);
+		assertEquals(a, b);
+		assertEquals(b, a);
+		
+		a = new Coordinate(1, 3);
+		b = new Coordinate(1, 3);
+		assertEquals(a, b);
+		assertEquals(b, a);
+	}
 
 }
