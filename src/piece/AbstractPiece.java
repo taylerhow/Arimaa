037dab5e7cd3c6b605744b728ca40e1a06c18b68
Philip Ross <rosspa@rose-hulman.edu>
2016-04-13 14:19:50 -0400

    removed coordinate from piece

--- src/piece/AbstractPiece.java
+++ src/piece/AbstractPiece.java
@@ -2,17 +2,15 @@ package piece;
 
 import java.awt.Image;
 
-public abstract class AbstractPiece implements Cloneable{
+public abstract class AbstractPiece {
 	private Image image;
 	private Owner owner;
 	private int rank;
-	private Coordinate coordinate;
 
-	public AbstractPiece(Image image, Owner owner, int rank, Coordinate coordinate) {
+	public AbstractPiece(Image image, Owner owner, int rank) {
 		this.image = image;
 		this.owner = owner;
 		this.rank = rank;
-		this.coordinate = coordinate;
 	}
 
 	public Image getImage() {
@@ -31,43 +29,21 @@ public abstract class AbstractPiece implements Cloneable{
 		return rank;
 	}
 
-	public Coordinate getCoordinate() {
-		return this.coordinate;
-	}
-
-	public void setCoordinate(Coordinate newCoordinate) {
-		if (newCoordinate.isValid()) {
-			this.coordinate = newCoordinate;
-		}
-	}
-
 	public boolean equals(Object obj) {
 		// this allows for subclassing of individual pieces
 		// can i treat the given object as this type of object != this.class == obj.class
 		if (obj.getClass().isAssignableFrom(this.getClass())) {
 			AbstractPiece other = (AbstractPiece) obj;
-			return this.getOwner().equals(other.getOwner()) && this.getCoordinate().equals(other.getCoordinate());
+			return this.getOwner().equals(other.getOwner());
 		}
 		return false;
 	}
 
 	public int hashCode() {
-		return this.getClass().hashCode() + this.image.hashCode() + this.owner.hashCode() + Integer.hashCode(this.rank) + this.coordinate.hashCode();
+		return this.getClass().hashCode() + this.image.hashCode() + this.owner.hashCode() + Integer.hashCode(this.rank);
 	}
 
 	public boolean isStrongerThan(AbstractPiece p2) {
 		return (this.getRank() > p2.getRank());
 	}
-	
-	public AbstractPiece clone() {
-		try {
-			AbstractPiece clone = (AbstractPiece) super.clone();
-			return clone;
-		} catch (CloneNotSupportedException e) {
-			e.printStackTrace();
-			return null;
-		}
-		
-	}
-
 }
--- src/piece/Rabbit.java
+++ src/piece/Rabbit.java
@@ -3,8 +3,8 @@ package piece;
 import javax.swing.ImageIcon;
 
 public class Rabbit extends AbstractPiece {
-	public Rabbit(Owner owner, Coordinate coordinate) {
-		super(null, owner, 0, coordinate);
+	public Rabbit(Owner owner) {
+		super(null, owner, 0);
 		String color = owner.equals(Owner.Player1) ? "White" : "Black";
 		this.setImage(new ImageIcon("resources/" + color + " rabbit.png").getImage());
 
